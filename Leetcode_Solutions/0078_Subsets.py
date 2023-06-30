cd6abda609e450b6222545848e9d1b78c3e46549
damian gacic <84860151+DamianGacic@users.noreply.github.com>
2023-06-30 16:59:49 +0200

    Create 0078_Subsets.py

---
+++ Leetcode_Solutions/0078_Subsets.py
@@ -0,0 +1,40 @@
+'''
+Given an integer array nums of unique elements, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+Example 1:
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+
+Example 2:
+Input: nums = [0]
+Output: [[],[0]]
+ 
+
+Constraints:
+1 <= nums.length <= 10
+-10 <= nums[i] <= 10
+All the numbers of nums are unique.
+'''
+
+# Using Backtracking
+
+class Solution:
+    
+    def backtrack(self, nums, res, cur, index):
+        if index > len(nums):
+            return
+        res.append(cur.copy())
+        for i in range(index, len(nums)):
+            if nums[i] not in  cur:
+                cur.append(nums[i])
+                self.backtrack(nums, res, cur, i)
+                cur.pop()
+        return
+        
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        cur = []
+        res = []
+        self.backtrack(nums, res, cur, 0)
+        return res
